353d2ad91bf77af26393234ee9d3bc8862d7e302
rw-denismbugua <denis@realware.com>
2023-07-09 13:29:44 +0300

    APTA-295 made request method

---
+++ AptaEvents.Module/Helpers/EventsApi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace AptaEvents.Module.Helpers
+{
+    public class EventsApi
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static async Task<List<string>> GetEventsAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                logger.Info($"Getting events from : {url}");
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var results = await response.Content.ReadAsStringAsync();
+
+                    List<string> events = JsonConvert.DeserializeObject<List<string>>(results);
+                    return events;
+                }
+                else
+                {
+                    logger.Error($"Error getting events list: {response.StatusCode}");
+                    throw new HttpRequestException($"Request failed with status code {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
---
+++ AptaEvents.Module/Services/Events.cs
@@ -0,0 +1,20 @@
+﻿using AptaEvents.Module.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AptaEvents.Module.Services
+{
+    public class Events
+    {
+        public async Task<List<string>> GetEvents()
+        {
+            List<string> events = await EventsApi.GetEventsAsync("url");
+            //Call the events api
+
+            return events;
+        }
+    }
+}
