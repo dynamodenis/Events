a3508669230802ec10bdb76d46a843515cb21fe6
oliver <oliver@realware.com>
2023-07-10 18:31:50 +0300

    APTA-347 | adding publish data property

---
+++ AptaEvents.Module/Migrations/20230710152950_EventPublishData.cs
@@ -0,0 +1,29 @@
+﻿using AptaEvents.Module.DTO;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AptaEvents.Module.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventPublishData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<PublishData>(
+                name: "PublishData",
+                table: "Events",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishData",
+                table: "Events");
+        }
+    }
+}
