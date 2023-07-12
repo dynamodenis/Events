58fc0aa41217588d24fc05d34ca48aa62a20135f
rw-daniel <134268324+rw-daniel@users.noreply.github.com>
2023-07-12 14:52:26 +0300

    APTA-405 field expiry

---
+++ AptaEvents.Module/Migrations/20230712101801_AddEventFieldEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AptaEvents.Module.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventFieldEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Expiry",
+                table: "EventFields",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Expiry",
+                table: "EventFields");
+        }
+    }
+}
